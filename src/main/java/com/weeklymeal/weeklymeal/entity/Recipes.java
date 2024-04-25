f8f68052b98f6bd9387267987691db2c5a5a29aa
JereDAM <usr3593@salesianos-lacuesta.net>
2024-04-25 10:03:06 +0100

    repositoires done

--- src/main/java/com/weeklymeal/weeklymeal/entity/Recipes.java
+++ src/main/java/com/weeklymeal/weeklymeal/entity/Recipes.java
@@ -10,12 +10,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "recipes")
 public class Recipes {
---
+++ src/main/java/com/weeklymeal/weeklymeal/repository/RecipesRepository.java
@@ -0,0 +1,11 @@
+package com.weeklymeal.weeklymeal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.weeklymeal.weeklymeal.entity.Recipes;
+
+@Repository
+public interface RecipesRepository extends JpaRepository<Recipes, Long>{
+
+}
