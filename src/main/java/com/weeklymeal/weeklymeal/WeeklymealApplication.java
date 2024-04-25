19ec01b870a6d7528c09005c9c15a372d2bccd10
JereDAM <usr3593@salesianos-lacuesta.net>
2024-04-25 09:40:54 +0100

    initial commit

---
+++ src/main/java/com/weeklymeal/weeklymeal/WeeklymealApplication.java
@@ -0,0 +1,13 @@
+package com.weeklymeal.weeklymeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklymealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklymealApplication.class, args);
+	}
+
+}
