091b0ca72d92ecdd19378a054826ef8f4b6e71a2
JereDAM <usr3593@salesianos-lacuesta.net>
2024-05-19 20:59:38 +0100

    Feature: Created userDto

---
+++ src/main/java/com/weeklymeal/weeklymeal/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.weeklymeal.weeklymeal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserDto {
+	
+    private Long id;
+    private String username;
+    private String email;
+
+}
---
+++ src/main/java/com/weeklymeal/weeklymeal/dto/UserDtoMapper.java
@@ -0,0 +1,11 @@
+package com.weeklymeal.weeklymeal.dto;
+
+import com.weeklymeal.weeklymeal.entity.User;
+
+public class UserDtoMapper {
+
+	public static UserDto toUserDTO(User user) {
+        return new UserDto(user.getId(), user.getUserName(), user.getEmail());
+    }
+
+}
